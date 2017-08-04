11a82411a7e00067ff18324bcc43981ea378815b
ixzus <iandroid@foxmail.com>
2017-08-04 22:10:44 +0800

    update

--- app/src/main/java/com/yltx/appcn/login/LoginActivity.java
+++ app/src/main/java/com/yltx/appcn/login/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.yltx.appcn.login;
 
 import android.os.Bundle;
+import android.support.animation.DynamicAnimation;
+import android.support.animation.SpringAnimation;
+import android.support.animation.SpringForce;
+import android.view.View;
+import android.widget.Button;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.yltx.appcn.R;
@@ -8,17 +13,20 @@ import com.yltx.appcn.base.BaseActivity;
 import com.yltx.appcn.base.BaseModel;
 
 @Route(path = "/login/loginActivity")
-public class LoginActivity extends BaseActivity<LoginContract.ILoginView, LoginPersenter> implements LoginContract.ILoginView {
+public class LoginActivity extends BaseActivity<LoginContract.ILoginView, LoginPersenter> implements LoginContract.ILoginView, View.OnClickListener {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(this);
     }
 
     @Override
     protected void initView() {
-
     }
 
     @Override
@@ -49,5 +57,20 @@ public class LoginActivity extends BaseActivity<LoginContract.ILoginView, LoginP
     @Override
     public void onLoginResult(String code) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                SpringForce spring = new SpringForce(0)
+                        .setDampingRatio(0.05f)
+                        .setStiffness(SpringForce.STIFFNESS_VERY_LOW);
+                final SpringAnimation anim = new SpringAnimation(mButton ,SpringAnimation.TRANSLATION_Y).setSpring(spring);
+                anim.cancel();
+                anim.setStartValue(-700);
+                anim.start();
+                break;
+        }
     }
 }
